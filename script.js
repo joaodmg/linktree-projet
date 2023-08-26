ed78a9112f389c53de0afd0d6533d5dbe208f8eb
João Vitor Godinho <joaovitordmg@gmail.com>
2023-08-26 09:41:05 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,27 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  /*if(html.classList.contains('light')){
+    html.classList.remove('light')
+  } else {
+    html.classList.add('light')
+  }*/
+
+  const img = document.querySelector("#profile img")
+
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute(
+      "alt",
+      "Foto de João Vitor sorrindo usando blusa amarela sentando em um restaurante"
+    )
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+    img.setAttribute(
+      "alt",
+      "Foto de João Vitor sorrindo usando blusa marrom sentando em um restaurante"
+    )
+  }
+
+  
+}
